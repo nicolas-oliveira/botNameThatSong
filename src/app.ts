aa11a9469294b9c687ce440a12712ce27ec30ce3
root <root@ESTOQUED101.localdomain>
2021-12-09 11:30:49 -0300

    config: add configuration files eslint and gitignore

--- src/app.ts
+++ src/app.ts
@@ -2,20 +2,20 @@ import { Client } from "@zenvia/sdk";
 import createWebHook from "./controllers/webhook-controller";
 
 class App {
-  /*
-  TODO: Add Logger, Add Dashboard Controller, Streamline a little more
-  */
+    /*
+     *TODO: Add Logger, Add Dashboard Controller, Streamline a little more
+     */
 
-  public async start() {
-    const client = new Client(process.env.ZENVIA_TOKEN);
-    const channel = client.getChannel(process.env.CHANNEL as any);
-    const webhook = await createWebHook(channel);
-    webhook.on("listening", () => {
-      console.info("Webhook is listening");
-    });
+    public async start() {
+        const client = new Client(process.env.ZENVIA_TOKEN),
+            channel = client.getChannel(process.env.CHANNEL as any),
+            webhook = await createWebHook(channel);
+        webhook.on("listening", () => {
+            console.info("Webhook is listening");
+        });
 
-    webhook.init();
-  }
+        webhook.init();
+    }
 }
 
 export default App;
