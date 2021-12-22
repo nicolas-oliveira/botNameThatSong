4804d7c8db60bdf695122f380f982c0b5ab43987
Nicolas Oliveira <nicolasm15@hotmail.com>
2021-12-22 17:14:05 -0300

    fix: change sampleRateHertz to 16000, change: add treatment to message error to user

--- src/controllers/gcp-controller.ts
+++ src/controllers/gcp-controller.ts
@@ -1,12 +1,11 @@
-import { protos, v1p1beta1 as speech } from '@google-cloud/speech';
-import { google } from '@google-cloud/speech/build/protos/protos';
-import getAndEncode from '../utils/base64-download';
-
+import { protos, v1p1beta1 as speech } from "@google-cloud/speech";
+import { google } from "@google-cloud/speech/build/protos/protos";
+import getAndEncode from "../utils/base64-download";
 
 // Init Types
-type SpeechRequest = protos.google.cloud.speech.v1p1beta1.ILongRunningRecognizeRequest;
+type SpeechRequest =
+    protos.google.cloud.speech.v1p1beta1.ILongRunningRecognizeRequest;
 type RecognitionConfig = google.cloud.speech.v1p1beta1.IRecognitionConfig;
-
 
 async function transcript(url: string) {
     try {
@@ -14,15 +13,15 @@ async function transcript(url: string) {
 
         const client = new speech.SpeechClient();
 
-        const sampleRateHertz = 48000;
-        const languageCode = 'en-US';
-        const alternativeLanguageCodes = ['en-US', 'pt-BR'];
+        const sampleRateHertz = 16000;
+        const languageCode = "en-US";
+        const alternativeLanguageCodes = ["en-US", "pt-BR"];
 
         const config: RecognitionConfig = {
             encoding: "OGG_OPUS", // Whatsapp's Encoding
             sampleRateHertz, // Whatsapp's Sample Rate
             languageCode, // Primary Code
-            alternativeLanguageCodes // Two languages we're using
+            alternativeLanguageCodes, // Two languages we're using
         };
 
         const audio = {
@@ -36,11 +35,10 @@ async function transcript(url: string) {
 
         const [operation]: any = await client.longRunningRecognize(request);
         const [response] = await operation.promise();
-        const transcription: string[] = response.results
-
-            .map((result: {
-                alternatives: { transcript: any; }[];
-            }) => result.alternatives[0].transcript)
+        const transcription: string[] = response.results.map(
+            (result: { alternatives: { transcript: any }[] }) =>
+                result.alternatives[0].transcript,
+        );
 
         return transcription;
     } catch (error) {
