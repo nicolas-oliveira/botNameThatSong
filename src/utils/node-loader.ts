7e098a23688a079db8123f40666e48ee4dfc71e7
hastoontests <flavouredsoda@gmail.com>
2022-01-10 01:41:14 -0300

    Fix: Fixed Node Registry in Production

--- src/utils/node-loader.ts
+++ src/utils/node-loader.ts
@@ -1,15 +1,29 @@
 function loadAllNodes(): void {
     const allModules: { [key: string]: NodeRequire } = {};
-    require("fs")
-        .readdirSync(__dirname + "/../nodeflow/")
-        .forEach(function (file) {
-            if (file.match(/\.ts$/) !== null && file !== "index.ts") {
-                var name = file.replace(".ts", "");
-                allModules[name] = require("./../nodeflow/" + file);
-            }
-        });
-    for (const module in allModules) {
-        new (allModules[module] as any).default();
+    if (process.env.NODE_ENV == "dev") {
+        require("fs")
+            .readdirSync(__dirname + "/../nodeflow/")
+            .forEach(function (file) {
+                if (file.match(/\.ts$/) !== null && file !== "index.ts") {
+                    var name = file.replace(".ts", "");
+                    allModules[name] = require("./../nodeflow/" + file);
+                }
+            });
+        for (const module in allModules) {
+            new (allModules[module] as any).default();
+        }
+    } else {
+        require("fs")
+            .readdirSync(__dirname + "/../nodeflow/")
+            .forEach(function (file) {
+                if (file.match(/\.js$/) !== null && file !== "index.js") {
+                    var name = file.replace(".js", "");
+                    allModules[name] = require("./../nodeflow/" + file);
+                }
+            });
+        for (const module in allModules) {
+            new (allModules[module] as any).default();
+        }
     }
 }
 
