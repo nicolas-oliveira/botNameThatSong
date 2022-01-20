5bb921e9e54b8e211a4bdf4116a90bac39de5269
hastoontests <flavouredsoda@gmail.com>
2022-01-20 18:52:32 -0300

    Fix: WhatsApp List didnt show

--- src/nodeflow/21-menu-detalhes.ts
+++ src/nodeflow/21-menu-detalhes.ts
@@ -7,9 +7,31 @@ export default class MenuDetalhesNode extends AbstractNode {
     }
 
     public async run(input: UserInput): Promise<void> {
-        this.sendButtons(
-            "Você pode ver algumas coisas sobre a sua música:",
-            ["Ver letra", "Ouvir um pedacinho", "Tentar outra música"],
+        // this.sendButtons(
+        //     "Você pode ver algumas coisas sobre a sua música:",
+        //     ["Ver letra", "Ouvir um pedacinho", "Tentar outra música"],
+        // );
+        this.sendList(
+            "Você pode ver algumas coisas sobre a sua música :",
+            "Clique aqui",
+            "Resultado",
+            "O que você quer fazer?",
+            {
+                title: "Ver letra",
+                description: "Posso mandar a letra inteira pra você",
+            },
+            {
+                title: "Ouvir um pedacinho",
+                description: "Posso mandar um trecho da música pra você",
+            },
+            {
+                title: "Tentar outra música",
+                description: "Podemos tentar de novo",
+            },
+            {
+                title: "Playlist no Spotify",
+                description: "Topa colocar essa musica no spotify?",
+            },
         );
         // Awaits for user input
         this.setNextInteractionNode(22);
